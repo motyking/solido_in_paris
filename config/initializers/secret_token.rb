e9a813ab9277251718a28dadfdffcdcd5bcd8245
motyking <timothee.cissokho@gmail.com>
2014-01-07 14:06:51 +0100

    premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolidoInParis::Application.config.secret_key_base = '80d1cc6ae47f047273f1a28ea9a4b71a2bb69ca59f0974ec093fdc10428db6faeee2b80b1a136a6a36b4e9bd6dd49016bdaedbcb8a559f5a85a67b3b5671f704'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SolidoInParis::Application.config.session_store :cookie_store, key: '_solido_in_paris_session'
